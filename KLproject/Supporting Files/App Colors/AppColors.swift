d74b472df2a6b2db4354625f73ef8a25696c7008
Krzysztof Lech <krzysztof.lech@24abc.pl>
2021-08-28 14:51:24 +0200

    SnapKit and colors added

---
+++ KLproject/Supporting Files/App Colors/AppColors.swift
@@ -0,0 +1,19 @@
+//
+//  AppColors.swift
+//  Miquido
+//
+//  Created by KL on 28/08/2021.
+//
+
+import UIKit
+
+struct AppColor {
+    static let background = UIColor(named: "background")
+    static let text = UIColor(named: "text")
+    
+    static let cellBackground = UIColor(named: "cellBackground")
+    static let cellContent = UIColor(named: "cellContent")
+    
+    static let detailsBackground = UIColor(named: "detailsBackground")
+    static let detailsContent = UIColor(named: "detailsContent")
+}
